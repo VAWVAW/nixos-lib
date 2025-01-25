c33c7770ac5ad5469d0f0855a6f72c62807607a8
vaw <valentin@wiedekind1.de>
2025-01-25 23:08:35 +0100

    gpg: Remove some unused settings

--- home/gpg.nix
+++ home/gpg.nix
@@ -20,17 +20,11 @@
         s2k-digest-algo = lib.mkDefault "SHA512";
         s2k-cipher-algo = lib.mkDefault "AES256";
         charset = lib.mkDefault "utf-8";
-        fixed-list-mode = lib.mkDefault true;
-        no-comments = lib.mkDefault true;
-        no-emit-version = lib.mkDefault true;
         no-greeting = lib.mkDefault true;
         keyid-format = lib.mkDefault "0xlong";
-        list-options = lib.mkDefault "show-uid-validity";
-        verify-options = lib.mkDefault "show-uid-validity";
         with-fingerprint = lib.mkDefault true;
         require-cross-certification = lib.mkDefault true;
         no-symkey-cache = lib.mkDefault true;
-        use-agent = lib.mkDefault true;
         throw-keyids = lib.mkDefault true;
       };
       scdaemonSettings.disable-ccid = lib.mkDefault true;
